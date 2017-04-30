e67d65d0e93d97d4ceee27205dcfb87411d2d9a5
Yuqian Yang <yyang@freewheel.tv>
2017-04-30 22:24:37 +0800

    encoder and decodeer

--- service/service.go
+++ service/service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"bytes"
+	"github.com/hashicorp/go-msgpack/codec"
 	"github.com/yangyuqian/gateway/types"
 )
 
@@ -11,4 +13,21 @@ type Service struct {
 	Upstream       *types.Upstream       // upstream can be nil
 	Authenticators []types.Authenticator // client attempt is accept when there is at least one Authenticator accepts it
 	Throttlers     []types.Throttler     // client attempt is limited to min rate
+}
+
+// encode object to raw data as payload
+func (svc *Service) Encode() (data []byte, err error) {
+	buf := bytes.NewBuffer([]byte{})
+	encoder := codec.NewEncoder(buf, &codec.SimpleHandle{})
+	if err = encoder.Encode(svc); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+// decode raw data into structural object
+func (svc *Service) Decode(raw []byte) (err error) {
+	decoder := codec.NewDecoder(bytes.NewReader(raw), &codec.SimpleHandle{})
+	return decoder.Decode(svc)
 }
