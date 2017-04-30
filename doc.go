fc446966b981b82695c4cd7c7215283dba32261a
Yuqian Yang <yyang@freewheel.tv>
2017-04-30 19:31:55 +0800

    generate grpc stub from protobuf spec

--- doc.go
+++ doc.go
@@ -4,4 +4,4 @@ package gateway
 // goto https://developers.google.com/protocol-buffers/docs/gotutorial for more
 // details on go protobuf, grpc is a protocol based on HTTP/2, and protobuf is
 // a customized text format encoded for effient marshalling/unmarshalling.
-//go:generate protoc --gogo_out=import_path=github.com/yangyuqian/gateway/store,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogopoto:. ./store/service.proto
+//go:generate protoc -I store/ store/service.proto --go_out=plugins=grpc:store
